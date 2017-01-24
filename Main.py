d3553bcc230523f71cf15479bd6e949d17f594c3
jonny Castle <castlez@live.com>
2017-01-24 15:28:53 -0800

    added skeleton to main

---
+++ Main.py
@@ -0,0 +1,16 @@
+
+
+def main():
+    print("Get it started in hah...")
+
+    # init
+
+    # until epoch or accuracy limit
+
+        # train through sets
+
+            # feedforward
+
+            # backpropagate
+
+        # test network
